eb1dd5954de8e9031ab2ae6285d140fb52fbce7f
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-17 13:17:18 -0400

    first commit

---
+++ src/main/java/com/tts/week14day1/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.tts.week14day1.controller;
+
+import com.tts.week14day1.model.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/books")
+    public String getBooks(Model model) {
+        List<Book> bookList = List.of(
+                new Book(1L, "title one", "author one"),
+                new Book(2l, "title two", "author two")
+        );
+        model.addAttribute("books", bookList);
+        return "greeting";
+    }
+
+
+
+    }
+
